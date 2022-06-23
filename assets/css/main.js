113fe1b1412bbea242951137c85ac06eb61ff3c9
haiyt1312 <haiyt1312@gmail.com>
2022-06-23 17:21:31 +0700

    update23/6

---
+++ assets/css/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $('.navbar').addClass('sticky');
+        }else{
+            $('.navbar').removeClass('sticky');
+        }
+    });
+
+    if($(window).width() > 100) {
+		$(window).on('scroll', function() {
+			if($(this).scrollTop() > 500) {
+				$('#btnTop').fadeIn();
+			} else {
+				$('#btnTop').fadeOut();
+			}
+		});	
+	}
+});
+
